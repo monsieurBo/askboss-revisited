e238d519c6a57af6775be0079018fffa06cd418f
monsieurBo <boscoho.hy@gmail.com>
2018-05-16 12:31:50 +0800

    WIP

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,36 +3,31 @@ class VotesController < ApplicationController
 
   def vote
   	byebug
-  	if @votes.nil?
-  		Vote.create(vote_from_params)
+  	if @vote.nil?
+  		if params[:question_id].nil?
+  		 Vote.create(answer_id: params[:answer_id], user_id: current_user.id)
+  		 redirect_to question_path(@vote.answer.question.id)
+  		elsif params[:answer_id].nil?
+  		 Vote.create(question_id: params[:question_id], user_id: current_user.id)
+  		 redirect_to question_path(@vote.question_id)
+  		end
     else
-	    if @votes.vote.nil?
-	      @votes.vote = true
-	    elsif @votes.vote?
-	      @votes.vote = false
+    	@question = @vote.question || @vote.answer.question
+	    if @vote.vote.nil?
+	      @vote.update(vote: true)
+	    elsif @vote.vote?
+	      @vote.update(vote: false)
 	    else
-	      @votes.vote = true
+	      @vote.update(vote: true)
 	    end 
 	  end  
-	  render :question     
+	  redirect_to question_path(@question.id)
   end
-
-    def vote_from_params
-	  vote_params = params[:vote] || Hash.new
-	  answer_id = vote_params.delete(:answer_id)
-	  question_id = vote_params.delete(:question_id)
-	  user_id = vote_params.delete(:user_id)
-	  vote = vote_params.delete(:vote)
-	end
 
   private 
 
   def set_vote
-  	@votes = Vote.find_by(answer_id: params[:answer_id], user_id: current_user.id) || Vote.find_by(question_id: params[:question_id], user_id: current_user.id)
+  	@vote = Vote.find_by(answer_id: params[:answer_id], user_id: current_user.id) || Vote.find_by(question_id: params[:question_id], user_id: current_user.id)
   end
-
-  def vote_params
-      params.require(:vote).permit(:answer_id, :user_id, :question_id, :vote)
-    end
 
 end
