e52c9daf1f9880d6ec646c4aa600c265415b482c
monsieurBo <boscoho.hy@gmail.com>
2018-05-20 17:13:55 +0800

    commit to rebase

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,12 @@ class User < ApplicationRecord
   has_many :votes
   has_many :flash_cards
   has_many :notes
-  
+  has_many :quizzes
+  has_many :follower_relationships, foreign_key: :following_id, class_name: 'Follow'
+  has_many :followers, through: :follower_relationships, source: :follower
+
+  has_many :following_relationships, foreign_key: :follower_id, class_name: 'Follow'
+  has_many :following, through: :following_relationships, source: :following
   include Clearance::User
 end
 
