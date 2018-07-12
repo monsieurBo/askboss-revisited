d5d292ebddb1e9bbf3b8417905158ab4624145c3
monsieurBo <boscoho.hy@gmail.com>
2018-07-12 18:08:10 +0800

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -66,16 +66,6 @@ ActiveRecord::Schema.define(version: 2018_05_21_080540) do
     t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
-  create_table "follows", force: :cascade do |t|
-    t.integer "following_id", null: false
-    t.integer "follower_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["follower_id"], name: "index_follows_on_follower_id"
-    t.index ["following_id", "follower_id"], name: "index_follows_on_following_id_and_follower_id", unique: true
-    t.index ["following_id"], name: "index_follows_on_following_id"
-  end
-
   create_table "personal_messages", force: :cascade do |t|
     t.text "body"
     t.bigint "conversation_id"
